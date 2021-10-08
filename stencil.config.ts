fb55eac58bed63ec78a7ad31592963b548489d3e
Matt Moore <Matt.M.Moore13@outlook.com>
2021-10-08 17:15:53 -0400

    Updated the react components-library dependencies

--- stencil.config.ts
+++ stencil.config.ts
@@ -1,8 +1,14 @@
 import { Config } from '@stencil/core';
+import { reactOutputTarget } from '@stencil/react-output-target'
 
 export const config: Config = {
   namespace: 'stencil-starter-project-name',
   outputTargets: [
+    reactOutputTarget({
+      componentCorePackage: 'mattsstenciltest',
+      proxiesFile: '../component-library-react/src/components.ts',
+      includeDefineCustomElements: true,
+    })
     {
       type: 'dist',
       esmLoaderPath: '../loader',
